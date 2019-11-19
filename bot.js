90628adf48960815338e6debfa0229609b13bc68
RizenLord <rizenlordgaming@gmail.com>
2019-11-19 09:32:43 -0500

    Add base bot

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message =>
+{
+	if (message.content === config.prefix + 'help')
+	{
+		message.channel.send('Test');
+	}
+});
+
+client.login(config.token);
