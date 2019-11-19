f160292815aa6f10b1b931e35a03034c4adb6511
ProfessorCampi <stonefox317@gmail.com>
2019-11-19 09:37:56 -0500

    code format

--- bot.js
+++ bot.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./data/config.json");
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.once('ready', () =>
+{
+	console.log('Memermon is ready!');
 });
 
 client.on('message', message =>
